2dc51e765951b022428a263bc289f70ac996c2ba
MichaelRempe <michaeljrempe@gmail.com>
2020-03-20 15:32:54 -0400

    sturcutre panels 100% height

--- client/src/components/LeftPanel/index.js
+++ client/src/components/LeftPanel/index.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./style.css";
 
 const LeftPanel = (props)=>{
     return(
-        <div className="col-2">
+        <div className="col-2 leftPanel">
             {props.children}
         </div>
     )
