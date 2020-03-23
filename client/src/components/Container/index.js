8e2799be284b4859bc5dd810c0188b9f80e8e101
Khalil <khalilhall1221@gmail.com>
2020-03-23 16:58:17 -0400

    fixed selector

--- client/src/components/Container/index.js
+++ client/src/components/Container/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 function Container(props){
     return(
-        <div className="wrapper">{props.children}</div>
+        <div className="container-fluid wrapper">{props.children}</div>
     )
 }
 
