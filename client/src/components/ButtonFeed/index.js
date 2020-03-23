8796918e14feaafa2712b668c9e352bb4f7f092a
MichaelRempe <michaeljrempe@gmail.com>
2020-03-23 12:39:54 -0400

    basic navigation prior to UUID

--- client/src/components/ButtonFeed/index.js
+++ client/src/components/ButtonFeed/index.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
 
-
-const FeedButton = ()=>{
-    return(
-        //navigates user to Home/Landing Page
-        <a href="#" lassName="btn btn-outline-dark btn-lg btn-block" role="button">My Feed</a>
-    )
-}
+const FeedButton = () => {
+  return (
+    //navigates user their feed page:
+    <Link to="/feed/dev">
+      <button className="btn btn-outline-dark btn-lg btn-block" role="button">
+        My Feed
+      </button>
+    </Link>
+  );
+};
 
 export default FeedButton;
--- client/src/components/ButtonHome/index.js
+++ client/src/components/ButtonHome/index.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
 
-
-const HomeButton = ()=>{
-    return(
-        //navigates user to Home/Landing Page
-        <a href="#" className="btn btn-lg btn-block home" role="button">Home</a>
-    )
-}
+const HomeButton = () => {
+  return (
+    //navigates user to Home/Landing Page
+    <Link to="/">
+      <button className="btn btn-outline-dark btn-lg btn-block" role="button">
+        Home
+      </button>
+    </Link>
+  );
+};
 
 export default HomeButton;
