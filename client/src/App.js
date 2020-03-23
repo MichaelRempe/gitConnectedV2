aa03d88b7460b355b57f608149506bc3be1233c6
Katie Durga <katie.durga@gmail.com>
2020-03-23 12:11:16 -0400

    protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,9 @@ import EmployerProfile from "./pages/EmployerProfile";
 import CreateDeveloper from "./pages/Forms/CreateDeveloper";
 import CreateEmployer from "./pages/Forms/CreateEmployer";
 import PostJob from "./pages/Forms/PostJob";
+    // Errors
+import ErrorPage from "./pages/ErrorPage";
+import Unauth from "./pages/Unauth";
 
 import firebase from "firebase/app";
 import {
@@ -30,7 +33,8 @@ function App() {
     <>
     <FirebaseAuthProvider {...config} firebase={firebase}>
       <Router >
-        <div>
+        
+          <IfFirebaseAuthed>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path='/selectform' component={SelectForm} />
@@ -44,8 +48,30 @@ function App() {
             <Route exact path="/dev-create-account" component={CreateDeveloper} />
             <Route exact path="/emp-create-account" component={CreateEmployer} />
             <Route exact path="/postjob" component={PostJob} />
+            <Route component={ErrorPage}></Route>
           </Switch>
-        </div>
+       
+        </IfFirebaseAuthed>
+        <IfFirebaseUnAuthed>
+            {/* CAN WE JUST REPLACE THIS WITH ONE CATCHALL ROUTE? */}
+            <Switch>
+            <Route exact path="/" component={Home} />
+            {/* <Route exact path="/login" component={Home} /> */}
+            <Route exact path='/selectform' component={Unauth} />
+            {/* Developer Paths */}
+            <Route path="/feed/dev/" component ={Unauth} />
+            <Route path="/profile/dev" component={Unauth} />
+            {/* Employer Paths */}
+            <Route path="/feed/emp" component ={Unauth} />
+            <Route path="/profile/emp" component={Unauth} />
+            {/* Forms */}
+            <Route exact path="/dev-create-account" component={Unauth} />
+            <Route exact path="/emp-create-account" component={Unauth} />
+            <Route exact path="/postjob" component={Unauth} />
+            <Route component={ErrorPage}></Route>
+
+            </Switch>
+          </IfFirebaseUnAuthed>
       </Router>
       </FirebaseAuthProvider>
     </>
