586c309fda32fc39cfa210ec82f3fe5d5d64bce7
Khalil <khalilhall1221@gmail.com>
2020-03-25 13:45:19 -0400

    fixed pathing

--- client/src/components/HomePageNav/index.js
+++ client/src/components/HomePageNav/index.js
@@ -2,14 +2,22 @@ import React from "react";
 import "./style.css";
 import ButtonProfile from "../ButtonProfile";
 import ButtonFeed from "../ButtonFeed"
+import ButtonHome from "../ButtonHome"
+import LogoutButton from "../LogoutButton"
+
 
 function HomePageNav(props) {
-  return (
-    <div className="row nav">
-    <nav className="navbar fixed-top  justify-content-between" id="navbar">
-      <div className="col-10"><a className="navbar-brand" href="https://git-connected.com/"><h1 id="heading">Git Connected</h1></a></div>
-    </nav>
-    </div>
+  return (   
+      <div className="row nav">
+      <nav className="navbar fixed-top  justify-content-between" id="navbar">
+        <div className="col-7"><a className="navbar-brand" href="https://git-connected.com/"><h1 id="heading">Git Connected</h1></a></div>
+        <div className="col-1"><ButtonHome/></div>
+        <div className="col-1"><ButtonFeed/></div>
+        <div className="col-1"><ButtonProfile/></div>
+        <div className="col-2"><LogoutButton/></div>
+        
+      </nav>
+      </div>
   );
 }
 
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,23 +1,15 @@
 import React from "react";
 import "./style.css";
-import ButtonProfile from "../ButtonProfile";
-import ButtonFeed from "../ButtonFeed"
-import ButtonHome from "../ButtonHome"
-import LogoutButton from "../LogoutButton"
 
-function HomePageNav(props) {
+
+function NavBar(props) {
   return (
     <div className="row nav">
     <nav className="navbar fixed-top  justify-content-between" id="navbar">
-      <div className="col-7"><a className="navbar-brand" href="https://git-connected.com/"><h1 id="heading">Git Connected</h1></a></div>
-      <div className="col-1"><ButtonHome/></div>
-      <div className="col-1"><ButtonFeed/></div>
-      <div className="col-1"><ButtonProfile/></div>
-      <div className="col-2"><LogoutButton/></div>
-      
+      <div className="col-10"><a className="navbar-brand" href="https://git-connected.com/"><h1 id="heading">Git Connected</h1></a></div>
     </nav>
     </div>
   );
 }
 
-export default HomePageNav;
+export default NavBar;
